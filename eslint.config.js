3a7b51aed8aff37aa9a4d9a7413bf0fa8adc6eff
hmndzzl <men241265@uvg.edu.gt>
2026-05-24 23:02:11 -0600

    chore: crear configuraciin de ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,33 @@
-import js from '@eslint/js'
-import globals from 'globals'
+import neostandard from 'neostandard'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import globals from 'globals'
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default [
+  ...neostandard({
+    ts: true,
+  }),
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      globals: globals.browser,
-    },
+    ignores: ['dist', 'node_modules', 'storybook-static']
   },
-])
+  {
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      }
+    },
+    rules: {
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'react/jsx-handler-names': 'off',
+      semi: ['error', 'never'],
+      'max-len': ['error', { code: 120 }]
+    }
+  }
+]
